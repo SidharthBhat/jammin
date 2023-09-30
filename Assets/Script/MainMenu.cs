fcf10d3b2b5fb9391e331fee004c722508726ad2
pestydesperado248 <58351647+pestydesperado248@users.noreply.github.com>
2023-09-29 23:19:38 -0500

    Added Circle Wipe and extra polish for menu

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public CanvasGroup Canvas;
+    public CircleWipeController circleWipeController;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,11 +23,38 @@ public class MainMenu : MonoBehaviour
 
     public void LoadStart()
     {
-        SceneManager.LoadScene("Game");
+        StartCoroutine(StartRoutine());
     }
 
     public void Exit()
     {
         Application.Quit();
+    }
+
+    IEnumerator StartRoutine()
+    {
+        float Time_current = 0;
+
+        float FadeTime = 0.5f;
+        float WipeTime = 2f;
+
+        while (Time_current <= FadeTime)
+        {
+            Canvas.alpha = (FadeTime - Time_current / FadeTime);
+            Time_current += Time.deltaTime;
+            yield return null;
+        }
+        Time_current = 0;
+
+
+        circleWipeController.FadeOut();
+        while (Time_current <= WipeTime)
+        {
+            Time_current += Time.deltaTime;
+            yield return null;
+        }
+
+        SceneManager.LoadScene("Game");
+        yield return null;
     }
 }
