c1336e32909b29c5caffe9acedd8739e74711646
SidharthBhat <56647919+SidharthBhat@users.noreply.github.com>
2023-10-01 15:53:28 +0100

    refactor and partial detect code
    big issue with fov angle being off, need to fix asap

--- Assets/Sid's Work/Enemy/Patrol.cs
+++ Assets/Sid's Work/Enemy/Patrol.cs
@@ -9,14 +9,18 @@ public class Patrol : MonoBehaviour
     [SerializeField] private Transform pfFov;
     public float fov;
     public float viewDist;
+    public float waitTime = 1f;
 
     public Transform[] patrolPoints;
     private int currentPoint;
     public float speed;
-    private float waitTime = 1f;
+    private float timer = 0;
     //basic gamestate, 0 idle, 1 moving, 2 detect, 3 distraction, 4 boxdetect, 5 boxreveal (lifting the box)
     private int state;
     private Vector3 direction;
+
+    public GameObject player;
+
 
     public Vector3 GetPosition()
     {
@@ -35,21 +39,34 @@ public class Patrol : MonoBehaviour
         fieldOfView=Instantiate(pfFov, null).GetComponent<FOV>();
         fieldOfView.SetFOV(fov);
         fieldOfView.SetViewDist(viewDist);
+        PatrolMove();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //add detect code here, if nothing patrol, otherwise activate game over. if distraction, do that
-        PatrolMove();
-        DetectPlayer();
+
+        //swap false with stun condition
+        if (false)
+        {
+            Stunned();
+        }
+        else
+        {
+            DetectPlayer();
+        }
+    }
+
+    private void Stunned()
+    {
+
     }
     
     private void PatrolMove()
     {
-        if (waitTime >= 0)
+        if (timer > 0)
         {
-            waitTime -= Time.deltaTime;
+            timer -= Time.deltaTime;
         }
         else
         {
@@ -62,7 +79,7 @@ public class Patrol : MonoBehaviour
             else
             {
                 currentPoint = (currentPoint + 1) % patrolPoints.Length;
-                waitTime = 1f;
+                timer = waitTime;
                 state = 0;
             }
         }
@@ -75,23 +92,45 @@ public class Patrol : MonoBehaviour
 
     private void DetectPlayer()
     {
-        //swap out zero for player position
-        if (Vector3.Distance(GetPosition(), Vector3.zero) < viewDist)
+        //PatrolMove();
+        //swap out out for player position function
+        if (Vector3.Distance(GetPosition(), player.transform.position) < viewDist)
         {
-            //swap zero for player position
-            Vector3 playerDirection = (Vector3.zero - GetPosition()).normalized;
+            //swap out for player position function
+            Vector3 playerDirection = (player.transform.position - GetPosition()).normalized;
             if(Vector3.Angle(direction, playerDirection) < fov / 2f)
             {
                 RaycastHit2D detect = Physics2D.Raycast(GetPosition(), playerDirection, viewDist);
                 if(detect != null)
                 {
+                    /*
                     //uncomment when player is added
-                    //if(detect.collider.gameObject.GetComponent<Player>() != null)
-                    //{
-                        //check detect states here
-                    //}
+                    if(detect.collider.gameObject.GetComponent<Player>() != null)
+                    {
+                        //state hasn't been added yet, so this can change
+                        //if we use two bools, one for move and one for box, i'll change this to if-else
+                        //currently running of assumed values, being:
+                        //0 default, 1 box, 2 boxmove
+                        int state = player.GetState();
+                        switch(state)
+                        {
+                            case 0:
+                                //game over
+                                break
+                            case 1:
+                                break
+                            case 2:
+                                //box suspicion
+                                //either they stop and look, then get back to it, or they just go to player and wait, and if player moves then they try to open the box, or they open the box anyways
+                                break
+                        }
+                    }
+                    */
+                    //remove this once detect code is implemented
+                    return;
                 }
             }
         }
+        PatrolMove();
     }
 }
